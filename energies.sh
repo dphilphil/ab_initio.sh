6203212fa83773913a4ca4fc0024084a55064977
Phillip Marks <phill.marks@yahoo.co.uk>
2017-06-27 14:02:33 +0100

    extracts final energies and any warnings from .castep files

---
+++ energies.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Written by https://github.com/dphilphil"
+
+
+for folder in */; do
+	cd $folder
+
+	#search for warnings	
+	if grep 'warn' *castep
+	  then echo "Warning Found @ $folder" >> '../warnings.log'
+	  else echo "Warning NotFound @ $folder" >> '../warnings.log'
+	fi
+	
+	#-n prevents carriage return (new line)
+	#-e allows escape characters to be read, used here to read \t as tab
+	echo -ne "$folder \t" >> '../energies.log'	
+	#grep finds line then pipe to awk to match word
+	grep "Final energy, E" *castep | awk '{print $5}' >> '../energies.log'
+	cd ../
+done
