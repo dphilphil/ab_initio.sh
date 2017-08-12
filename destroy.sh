cd6be44c45414895026ad60327b578bb89e4b2f5
Phillip Marks <phill.marks@yahoo.co.uk>
2017-08-12 10:50:06 +0100

    deletes all castep files except .cell and .param

---
+++ destroy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Written by https://github.com/dphilphil"
+
+
+for folder in */; do
+        cd $folder
+        echo "$folder"
+
+        #unhash as approriate
+        #rm *bands *bib *castep *bin *esp *usp *out* *check *geom
+        #rm *check
+        cd ../
+done
